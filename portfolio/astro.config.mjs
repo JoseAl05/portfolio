ddfae6dbadede3939de3db5d88e3b0cff7e21ed1
JoseAl05 <jparancibialinker@gmail.com>
2023-07-12 20:20:52 -0400

    added vercel adapter

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -1,19 +1,18 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-import react from "@astrojs/react"
-import sanity from "astro-sanity"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import sanity from "astro-sanity";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    sanity({
-      projectId: 'xmea2rf5',
-      dataset: 'production',
-      apiVersion: '2023-02-08',
-      useCdn: false
-    })
-  ],
-  output: 'server'
-})
+  integrations: [tailwind(), react(), sanity({
+    projectId: 'xmea2rf5',
+    dataset: 'production',
+    apiVersion: '2023-02-08',
+    useCdn: false
+  })],
+  output: 'server',
+  adapter: vercel()
+});
